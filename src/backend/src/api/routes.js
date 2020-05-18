f94a610a3ba10aee9701d99d0b9898b21816548b
Milos Marinkovic <hemicar92@gmail.com>
2020-05-18 22:23:16 +0200

    Folder structure fixed

--- src/backend/src/api/routes.js
+++ src/backend/src/api/routes.js
@@ -5,14 +5,7 @@ function sendEmail() {
     const router = express.Router();
 
     router.post('/send-email', (req, res, next) => {
-        // if (res) {
-        //     res.send('POST CALLED');
-        //     console.log('%crouter called: ', 'color: #00bfa5', res); 
-        // }
-
         res.send('POST CALLED');
-
-        // next();
     });
 
     return router;
--- src/backend/src/server.js
+++ src/backend/src/server.js
@@ -9,15 +9,12 @@ const _PORT_ = process.env.PORT || 8001;
 
 let app = express();
 
-app.use(express.static(path.join(__dirname, 'frontend')));
+app.use(express.static(path.join(__dirname, 'frontend/src')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use(routes.sendEmail());
 
-app.post('/send-email', (req, res) => {
-    res.send('POST CALLED!');
-})
+app.use(routes.sendEmail());
 
 app.listen(_PORT_, () => {
-    console.log(`%cServer started at: http://localhost:${_PORT_}`, 'color: #00bfa5');
+    console.log(`Server started at: http://localhost:${_PORT_}`);
 });
